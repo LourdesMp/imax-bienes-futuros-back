246a26f81e19b0da1d376ca8d802c686541a8f13
LourdesMp <lourdes.merino@imax.com.pe>
2020-10-23 12:21:35 -0500

    creando controllador para tasaciones

---
+++ src/controllers/tasacionController.js
@@ -0,0 +1,53 @@
+const con = require("./config");
+
+const newProyecto = async (req, rest) => {
+  
+    try {
+        var {IdProyecto,NombreProyecto,
+            Direccion,Promotor,Banco} = req.body;
+
+       
+
+        var sql = "INSERT INTO proyectos (IdProyecto,NombreProyecto,Direccion,Promotor,Banco) "+
+        "Values(?,?,?,?,?)";
+       // console.log(sql);
+        const result = await con.query(sql,[IdProyecto,NombreProyecto,Direccion,Promotor,Banco], 
+            async (err, res) => {
+            if (err) {
+                console.log(err);
+                return rest.status(404).json({ Data: [], Message: "Error en el servidor", StatusCode: 404 });
+            }
+            return rest.status(200).json({ Data: res, Message: "Proyecto enviado correctamente", StatusCode: 200 });
+        });
+    } catch (error) {
+        console.log(error);
+        return rest.status(500).json({ Data: error, Message: "Error en el servidor", StatusCode: 500 });
+    }
+}
+
+const listTasaciones = async (req, rest) => {
+ 
+    try {
+      //  var {DniSolicitante,Solicitante} = req.body;
+
+       
+
+        var sql = " SELECT * FROM tasaciones ";
+       // console.log(sql);
+        const result = await con.query(sql, async (err, res) => {
+            if (err) {
+                console.log(err);
+                return rest.status(404).json({ Data: [], Message: "Error en el servidor", StatusCode: 404 });
+            }
+            return rest.status(200).json({ Data: res, Message: "", StatusCode: 200 });
+        });
+    } catch (error) {
+        console.log(error);
+        return rest.status(500).json({ Data: error, Message: "Error en el servidor", StatusCode: 500 });
+    }
+}
+
+module.exports = {
+    newProyecto,
+    listTasaciones
+}
