54510b02bc65f7fd573d0181e75b7e2b074b6173
LourdesMp <lourdes.merino@imax.com.pe>
2020-10-23 12:22:34 -0500

    creando metodo para nuevo proyecto

--- src/routes/api/proyectos.js
+++ src/routes/api/proyectos.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const {createExpediente,listProyectos } = require("../../controllers/proyectosController");
+const { newProyecto, listProyectos } = require("../../controllers/proyectosController");
 
-router.post("/createexpediente", createExpediente);
+
+router.post("/newProyecto", newProyecto);
 router.get("/list", listProyectos);
+
 
 
 module.exports = router;
