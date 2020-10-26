e8eb660bae682ac0d03c7ffee6df12f8cef6c447
LourdesMp <lourdes.merino@imax.com.pe>
2020-10-26 17:02:23 -0500

    ruta psot

--- src/routes/api/tasaciones.js
+++ src/routes/api/tasaciones.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 
-const { listTasaciones } = require ("../../controllers/tasacionController");
+const { newTasacion, listTasaciones } = require ("../../controllers/tasacionController");
 
-
+router.post("/newTasacion", newTasacion);
 router.get("/listTasaciones", listTasaciones);
 
 
