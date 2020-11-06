21a28241c3df591ff9c1b2dc5af79321d68415ce
LourdesMp <lourdes.merino@imax.com.pe>
2020-11-05 22:13:40 -0500

    edit peticion post
    ya recibe files en la tabla

--- src/controllers/proyectosController.js
+++ src/controllers/proyectosController.js
@@ -1,18 +1,22 @@
 const con = require("./config");
+const multer = require ('multer');
+const path = require ('path')
 
-const newProyecto = async (req, rest) => {
-  
+
+const newProyecto =  (req, rest)=> {
+    // upload.single('file');
+    console.log(req.files);
     try {
-        var {idProyecto,nombreProyecto,
-            direccion,promotor,banco} = req.body;
-
-       
-
-        var sql = "INSERT INTO proyectos (idProyecto,nombreProyecto,direccion,promotor,banco) "+
-        "Values(?,?,?,?,?)";
+        var matriz =  'D:\\imax\\imax-bienes-futuros-back\\uploads\\'+ req.files.file[0].filename;
+        var tasacion = 'D:\\imax\\imax-bienes-futuros-back\\uploads\\' + req.files.tasacion[0].filename;
+        // console.log(req.body)
+        var  {nombreProyecto, direccion,promotor,banco} = req.body;
+   
+        var sql = "INSERT INTO proyectos (nombreProyecto,direccion,promotor,banco,matriz,tasacion) "+
+        "Values(?,?,?,?,?,?)";
        // console.log(sql);
-        const result = await con.query(sql,[idProyecto,nombreProyecto,direccion,promotor,banco], 
-            async (err, res) => {
+        const result =  con.query(sql,[nombreProyecto,direccion,promotor,banco,matriz, tasacion], 
+             (err, res) => {
             if (err) {
                 console.log(err);
                 return rest.status(404).json({ DataProjects: [], Message: "Error en el servidor", StatusCode: 404 });
@@ -23,7 +27,8 @@ const newProyecto = async (req, rest) => {
         console.log(error);
         return rest.status(500).json({ DataProjects: error, Message: "Error en el servidor", StatusCode: 500 });
     }
-}
+    // rest.send("Todo ok");
+};
 
 const listProyectos = async (req, rest) => {
  
