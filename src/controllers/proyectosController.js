ac354548b92f3070255b9732af2f939552b99d7b
LourdesMp <lourdes.merino@imax.com.pe>
2020-11-06 20:43:18 -0500

    agregando metodo para ID
    crear database

--- src/controllers/proyectosController.js
+++ src/controllers/proyectosController.js
@@ -5,23 +5,51 @@ const path = require ('path')
 
 const newProyecto =  (req, rest)=> {
     // upload.single('file');
-    console.log(req.files);
+    //  console.log(req.files);
     try {
-        var matriz =  'D:\\imax\\imax-bienes-futuros-back\\uploads\\'+ req.files.file[0].filename;
-        var tasacion = 'D:\\imax\\imax-bienes-futuros-back\\uploads\\' + req.files.tasacion[0].filename;
+        var  {idProyecto,nombreProyecto, direccion,promotor,banco} = req.body;
+        var nombreData = 'data-'+idProyecto + '.' + req.files.data[0].originalname.split('.').pop();
+            var nombreTasacion = 'tasacion-'+idProyecto  + '.' + req.files.tasacion[0].originalname.split('.').pop();
+        var data =  'D:\\imax\\imax-bienes-futuros-back\\uploads\\'+nombreData ;
+             var tasacion = 'D:\\imax\\imax-bienes-futuros-back\\uploads\\' + nombreTasacion;
         // console.log(req.body)
-        var  {nombreProyecto, direccion,promotor,banco} = req.body;
+       
    
-        var sql = "INSERT INTO proyectos (nombreProyecto,direccion,promotor,banco,matriz,tasacion) "+
+        var sql = "INSERT INTO proyectos (nombreProyecto,direccion,promotor,banco,data,tasacion) "+
         "Values(?,?,?,?,?,?)";
        // console.log(sql);
-        const result =  con.query(sql,[nombreProyecto,direccion,promotor,banco,matriz, tasacion], 
+        const result =  con.query(sql,[nombreProyecto,direccion,promotor,banco,data,tasacion], 
              (err, res) => {
+                 console.log(err)
             if (err) {
-                console.log(err);
+                
+
+
                 return rest.status(404).json({ DataProjects: [], Message: "Error en el servidor", StatusCode: 404 });
             }
+            var nombreDb = 'db_'+idProyecto;
+            var sql1  = 'CREATE DATABASE '+nombreDb;
+            var sql2 = 'USE ' +nombreDb;
+            var sql4 = ' CREATE TABLE matriz( idProyecto INT);';
+            var sql3 = ' CREATE TABLE tasacion( idProyecto INT);';
+            con.query(sql1,(err,res1) => {
+
+            });
+            con.query(sql2,(err,res2) => {
+
+            });
+            con.query(sql3,(err,res3) => {
+
+            });
+
+            con.query(sql4,(err,res4) => {
+
+            });
+
+
+            
             return rest.status(200).json({ DataProjects: res, Message: "Proyecto enviado correctamente", StatusCode: 200 });
+           
         });
     } catch (error) {
         console.log(error);
@@ -29,6 +57,28 @@ const newProyecto =  (req, rest)=> {
     }
     // rest.send("Todo ok");
 };
+
+const getLastId = async (req, rest) => {
+ 
+    try {
+      //  var {DniSolicitante,Solicitante} = req.body;
+
+       
+
+        var sql = " SELECT (idProyecto+1) AS LastID FROM proyectos ORDER BY idProyecto DESC LIMIT 1";
+       // console.log(sql);
+        const result = await con.query(sql, async (err, res) => {
+            if (err) {
+                console.log(err);
+                return rest.status(404).json({ DataProjects: [], Message: "Error en el servidor", StatusCode: 404 });
+            }
+            return rest.status(200).json({ DataProjects: res, Message: "", StatusCode: 200 });
+        });
+    } catch (error) {
+        console.log(error);
+        return rest.status(500).json({ DataProjects: error, Message: "Error en el servidor", StatusCode: 500 });
+    }
+}
 
 const listProyectos = async (req, rest) => {
  
@@ -54,5 +104,6 @@ const listProyectos = async (req, rest) => {
 
 module.exports = {
     newProyecto,
-    listProyectos
+    listProyectos,
+    getLastId
 }
